92cb2d340678b246762399d5b1b2dd2ea4f98454
Tim Bannister <tim@scalefactory.com>
2020-12-22 14:08:13 +0000

    Update example to match module

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,4 @@
-module "cloudfront_auth" {
+module cloudfront_auth {
   source                         = "../"
 
   auth_vendor                    = "github"
@@ -10,11 +10,11 @@ module "cloudfront_auth" {
 
   bucket_name                    = "private.example.com"
   region                         = "eu-west-1"
-  cloudfront_acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+  cloudfront_acm_certificate_arn = aws_acm_certificate.cert.arn
 }
 
-resource "aws_acm_certificate" "cert" {
-    provider                  = "aws.us-east-1"
+resource aws_acm_certificate cert {
+    provider                  = aws.us-east-1
     domain_name               = "example.com"
     validation_method         = "EMAIL"
     subject_alternative_names = [
@@ -23,10 +23,10 @@ resource "aws_acm_certificate" "cert" {
 }
 
 // A test object for the bucket.
-resource "aws_s3_bucket_object" "test_object" {
-    bucket       = "${module.cloudfront_auth.s3_bucket}"
+resource aws_s3_bucket_object test_object {
+    bucket       = module.cloudfront_auth.s3_bucket
     key          = "index.html"
     source       = "${path.module}/index.html"
     content_type = "text/html"
-    etag         = "${md5(file("${path.module}/index.html"))}"
+    etag         = md5(file("${path.module}/index.html"))
 }
