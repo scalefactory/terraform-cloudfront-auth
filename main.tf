1a4b251f1d0e1e860b7fb92d6c7ef1d026d1a933
Tim Bannister <tim@scalefactory.com>
2020-12-22 14:07:50 +0000

    Configure S3 Block Public Access

--- main.tf
+++ main.tf
@@ -72,6 +72,15 @@ resource "aws_s3_bucket" "default" {
     tags   = var.tags
 }
 
+# Block direct public access
+resource "aws_s3_bucket_public_access_block" "default" {
+    bucket = aws_s3_bucket.default.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    restrict_public_buckets = true
+}
+
 data "aws_iam_policy_document" "s3_bucket_policy" {
     statement {
         actions = [
