d35c9ba4e79fb91fa49ed4d3bf4e7942abb4548c
Ashton Kinslow <github@ashtonkinslow.com>
2021-10-21 14:30:23 -0500

    add domain name

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,7 @@ output "hosted_zone_id" {
   description = "hosted_zone_id "
   value       = aws_cloudfront_distribution.default.hosted_zone_id
 }
+
+output "domain_name" {
+  value = aws_cloudfront_distribution.default.domain_name
+}
